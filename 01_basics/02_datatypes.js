2ac6a9d894a162a6a21b716da6ab8b1532d379de
lonewarior001 <65966514+lonewarior001@users.noreply.github.com>
2023-10-17 08:36:08 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // teat all the JS codde as newer version 
+
+// alert(3+3) // WILL NOT WORK! AS WE ARE USING NODE INSTEAD OF BROWSER  
+
+console.log(3 
+    + 3) // Bad practice code should be more readable
+
+console.log("Harsh")
+
+let name = "Harsh"
+let age = 23
+let IsLoggedIn = false
+let state = null 
+let status;
+// number => 2 To the power of 53
+// bigint => numbers bigger than int
+// string => ""
+// boolean => true/false
+// null => standalone value
+// unidefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof name); // string
+console.log(typeof null); // object
